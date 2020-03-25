9821bd652f55949f7cb92c275719fb11d6a2cb31
mohammadsalkini <eng.salkini@hotmail.com>
2020-03-25 17:58:27 +0100

    add OrderController

--- src/main/java/com/mohammadalsalkini/tacocloud/controller/OrderController.java
+++ src/main/java/com/mohammadalsalkini/tacocloud/controller/OrderController.java
@@ -5,8 +5,12 @@ import com.mohammadalsalkini.tacocloud.model.Order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -15,9 +19,18 @@ public class OrderController {
 
     @GetMapping("/current")
     public String orderForm(Model model) {
-        log.info("Start orderForm method ...");
         model.addAttribute("order", new Order());
-        log.info("End orderForm method ...");
         return "orderForm";
     }
+
+    @PostMapping
+    public String processOrder(@Valid Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
+        log.info("Order submitted: " + order);
+        return "redirect:/";
+    }
 }
+

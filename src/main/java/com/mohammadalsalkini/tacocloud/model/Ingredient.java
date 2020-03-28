5f5af5aa42c00614f23a5fe260fb6fd506e8b312
mohammadsalkini <eng.salkini@hotmail.com>
2020-03-28 06:58:23 +0100

    update Ingredient class

--- src/main/java/com/mohammadalsalkini/tacocloud/model/Ingredient.java
+++ src/main/java/com/mohammadalsalkini/tacocloud/model/Ingredient.java
@@ -1,18 +1,21 @@
 package com.mohammadalsalkini.tacocloud.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor(access=AccessLevel.PUBLIC, force=true)
+@Entity
 public class Ingredient {
-
+    @Id
     private final String id;
     private final String name;
     private final Type type;
-
     public static enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
-
 }

fe852e9ec9e3e9159d5b06a2563ef35f4974a51c
mohammadsalkini <eng.salkini@hotmail.com>
2020-03-28 07:09:11 +0100

    update Order class

--- src/main/java/com/mohammadalsalkini/tacocloud/model/Order.java
+++ src/main/java/com/mohammadalsalkini/tacocloud/model/Order.java
@@ -2,8 +2,23 @@ package com.mohammadalsalkini.tacocloud.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 @Data
-public class Order {
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date placedAt;
+
     private String name;
     private String street;
     private String city;
@@ -13,7 +28,15 @@ public class Order {
     private String ccExpiration;
     private String ccCVV;
 
-    public void addDesign(Taco saved) {
-        
+    @ManyToMany(targetEntity = Taco.class)
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addDesign(Taco design) {
+        this.tacos.add(design);
+    }
+
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
     }
 }

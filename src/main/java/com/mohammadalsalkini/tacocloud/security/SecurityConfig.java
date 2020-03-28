25a239d100610ab2a292e02d2bc6707214b513fb
mohammadsalkini <eng.salkini@hotmail.com>
2020-03-28 11:12:14 +0100

    new update

--- src/main/java/com/mohammadalsalkini/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/mohammadalsalkini/tacocloud/security/SecurityConfig.java
@@ -1,26 +1,47 @@
 package com.mohammadalsalkini.tacocloud.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    //configureAuthentication_inMemory
+
+    @Qualifier("userRepositoryUserDetailsService")
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
         auth
-                .inMemoryAuthentication()
-                .withUser("mohammad")
-                .password("{noop}password")
-                .authorities("ROLE_USER")
-                .and()
-                .withUser("omran")
-                .password("{noop}password")
-                .authorities("ROLE_USER");
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
+
+//        //configureAuthentication_inMemory
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("mohammad")
+//                .password("{noop}password")
+//                .authorities("ROLE_USER")
+//                .and()
+//                .withUser("omran")
+//                .password("{noop}password")
+//                .authorities("ROLE_USER");
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new StandardPasswordEncoder("53cr3t");
     }
 
 

56b445080bf60f1899207db7773766f65afb88fb
mohammadsalkini <eng.salkini@hotmail.com>
2020-03-28 10:09:30 +0100

    In-memory user store security configuration

--- src/main/java/com/mohammadalsalkini/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/mohammadalsalkini/tacocloud/security/SecurityConfig.java
@@ -1,10 +1,27 @@
 package com.mohammadalsalkini.tacocloud.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //configureAuthentication_inMemory
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("mohammad")
+                .password("{noop}password")
+                .authorities("ROLE_USER")
+                .and()
+                .withUser("omran")
+                .password("{noop}password")
+                .authorities("ROLE_USER");
+    }
+
+
 }
